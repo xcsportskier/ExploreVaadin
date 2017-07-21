909494a71d4a0d272e3637cc0d46ba6bb55179e1
vaadin <vaadin@192.168.0.5>
2017-07-22 02:29:18 +0300

    Another commit

--- spring-boot-vaadin/src/main/java/com/udemy/vaadinspringboot/VaadinUI.java
+++ spring-boot-vaadin/src/main/java/com/udemy/vaadinspringboot/VaadinUI.java
@@ -2,9 +2,11 @@ package com.udemy.vaadinspringboot;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -16,8 +18,17 @@ public class VaadinUI extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 		VerticalLayout vl = new VerticalLayout();
+		TextField textField = new TextField("Text:");
+		textField.setValue("Some value");
+		textField.addValueChangeListener(e -> { 
+			Label label = new Label(e.getValue());
+			vl.addComponent(label);
+		});
+		textField.setValueChangeMode(ValueChangeMode.LAZY);
+		vl.addComponent(textField);
+		
 		vl.addComponent(new Label("You are here!!!"));
-		Button b = new Button("Click Me");
+		Button b = new Button("Click Me Once");
 		b.addClickListener(e -> vl.addComponent(new Label("Button have been clicked")));
 		vl.addComponent(b);
 		setContent(vl);
